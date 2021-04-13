39ef09e77f1b5fbbe97f70ed260b9522c5add082
noni227 <81325180+Noni2207@users.noreply.github.com>
2021-04-13 14:07:31 +0200

    create languages folder(translate)

---
+++ languages/en.php
@@ -0,0 +1,73 @@
+<?php
+$lang = array(
+     "title" => "Personal Portfolio Ninoslav Stanojevic",
+     "lang_en" => "",
+     "lang_no" => "",
+     "portfolio" => "Portfolio",
+     "about" => "About",
+     "service" => "Service",
+     "skills" => "Skills",
+     "project" => "Project",
+     "contact" => "Contact",
+     "text-1" => "Hello, my name is",
+     "text-2" => "Ninoslav Stanojevic",
+     "text-3" => "And I'm a",
+     "work" => "View my work",
+     "text-4" => "I'am a",
+     "pabout" => "As a Developer with a passion for coding and creating.
+     I enjoy learning new skills and technologies...<br> 
+     I am organized and love keeping my code organized as well.
+     I am innovative and creative ...<br> 
+     Constantly strives to deepen my knowledge in the field of software
+     development and to learn new tools and technologies...<br> 
+     I try to be a team player, a good coach and serve as a role model 
+     for everybody with the ability to handle different situations; i.e., demonstrate the versatility...<br>",
+     "aboutspan" => "I am a diligent, sociable, self-motivated and responsible team member...!",
+     "cv" => "Download CV",
+     "pservices1" => "Website design may consider it to be good practice to conform to standards. This is usually done via a description CSS...!",
+     "pservices2" => "To solve your business problems, I use a complete set of tools, from HTML5 to the latest JavaScript frameworks...!",
+     "pservices3" => "Inspired by challenges, problem-solving, and finding new architecture solutions. Including investigation, analysis with PHP...!",
+     "text-5" => "My creative",
+     "skillsspan1" => "skills",
+     "skillsspan2" => "experience...",
+     "html" => " 
+     I use to include  in web pages, video, audio, and even entire other webpages registering and logging in, sending feedback and more: dealing with titles, adding images or videos, emphasizing content, creating a basic form, etc.",
+     "css" => "
+     style and layout web pages — for example,  how to style text, and how to style and manipulate the boxes that your content sits inside. Different display settings, modern layout tools like flexbox, CSS grid, and positioning,
+     animations, and others.",
+     "js" => "
+     to adds interactivity to your website, with data entry on forms, dynamic styling, with animation your
+     websites will enter a new dimension of power and creativity.JavaScript itself is a relatively compact, yet very flexible language.",
+     "php" => "
+      I start to add an  open-source server-side scripting language that can be embedded into HTML to build web applications and dynamic websites.",
+     "mysql" => "
+     I use to handling & querying databases...",
+     "wp" => "
+     When the website is ready for build
+     I taking the graphical elements (colors, fonts, layout, images) defined in the design process and coding them using web
+     industry standards (PHP, HTML5, CSS3, JS, jQuery).",
+     "bbutton" => "View site",
+     "text-6" => "Get in Touch",
+     "desing" => "Simplicity is one of the golden rules of website design...",
+     "pcontact" => "I aim to attain an engaging internship position in the field of architecture solutions, full application development life cycle, including investigation, analysis, design, build, test, implement, maintenance and support...!",
+     "n" => "Name",
+     "a" => "Address",
+     "e" => "Email",
+     "h3contact" => "Contact Me",
+     "h4contact" => "Contact me today, and get reply with in 24 hours!",
+     "trmessage" => "Message send, thanks you for contacting me!",
+     "send" => "Send message ...",
+     "name1" => "Your name",
+     "email1" => "Your Email Address",
+     "subject1" => "Subject",
+     "textarea1" => "Message Here..., describe project..?",
+     
+
+     
+);
+
+
+
+
+
+?>
---
+++ languages/no.php
@@ -0,0 +1,75 @@
+<?php
+$lang = array(
+     "title" => "Personlig Portefølje Ninoslav Stanojevic",
+     "lang_en" => "",
+     "lang_no" => "",
+     "portfolio" => "Portefølje",
+     "about" => "Om",
+     "service" => "Service",
+     "skills" => "Ferdigheter",
+     "project" => "Prosjekt",
+     "contact" => "Kontakt",
+     "text-1" => "Hei, mitt navn er",
+     "text-2" => "Ninoslav Stanojevic",
+     "text-3" => "Og, jeg er en",
+     "work" => "Vis arbeidet mitt",
+     "text-4" => "Jeg er en,",
+     "pabout" => "Som utvikler med lidenskap for koding og oppretting.
+     Jeg liker å lære nye ferdigheter og teknologier ... <br>
+     Jeg er organisert og elsker å holde koden min også organisert.
+     Jeg er en innovativ og kreativ ... <br>
+     Arbeider kontinuerlig med å utdype min kunnskap innen programvareutvikling og å lære nye verktøy og teknologier ... <br>
+     Jeg prøver å være en lagspiller, en god trener, og tjene som et forbilde
+     for alle muligheten til å håndtere forskjellige situasjoner; dvs. demonstrere allsidighet. <br>",
+     "aboutspan" => "Jeg er et flittig, omgjengelig, selvmotivert og ansvarlig teammedlem ...!",
+     "cv" => "Last ned CV",
+     "pservices1" => "Nettside design kan vurdere det som god praksis å overholde standarder. Dette gjøres vanligvis via en beskrivelse CSS ...!",
+     "pservices2" => "For å løse dine forretningsproblemer bruker jeg et komplett sett med verktøy, fra HTML5 til de nyeste JavaScript-rammene ...!",
+     "pservices3" => "Inspirert av utfordringer, problemløsing og finne nye arkitekturløsninger. Inkludert etterforskning, analyse med PHP ...!",
+     "text-5" => "Min kreative",
+     "skillsspan1" => "ferdigheter",
+     "skillsspan2" => "erfaring...",
+     "html" => "
+     Jeg bruker å inkludere i websider, video, lyd og til og med hele andre nettsider som registrerer og logger inn, sender tilbakemelding og mer: å håndtere titler, legge til bilder eller videoer, vektlegge innhold, lage et grunnleggende skjema, etc.",
+     "css" => "
+     nettsider for stil og layout - for eksempel hvordan man styler tekst, og hvordan man styler og manipulerer boksene som
+     innholdet ditt sitter inne. Ulike skjerminnstillinger, moderne layoutverktøy som flexbox, CSS-rutenett og posisjonering,
+     animasjoner og annet.",
+     "js" => "
+     for å legge til interaktivitet på nettstedet ditt, med dataregistrering på skjemaer, dynamisk styling, med animasjon din
+     nettsteder vil komme inn i en ny dimensjon av kraft og kreativitet. JavaScript i seg selv er relativt kompakt, men likevel veldig fleksibelt språk.",
+     "php" => "
+     Jeg begynner å legge til åpen kildekode skriptspråk på serversiden som kan bygges inn i HTML for å bygge webapplikasjoner og dynamiske nettsteder.
+",
+     "mysql" => "
+     Jeg bruker med håndtering og utspørring av databaser",
+     "wp" => "
+     Når nettstedet er klart for bygging
+     Jeg tar de grafiske elementene (farger, skrifttyper, layout, bilder) definert i designprosessen og koder dem ved hjelp av nettet
+     industristandarder (PHP, HTML5, CSS3, JS, jQuery).",
+     "bbutton" => "Vis nettside",
+     "text-6" => "Ta kontakt med meg",
+     "desing" => "Enkelhet er en av de gyldne reglene for nettside design ...",
+     "pcontact" => "Jeg har som mål å oppnå en engasjerende praksisplass innen arkitekturløsninger, full levetid for applikasjonsutvikling, inkludert undersøkelse, analyse, design, konstruksjon, test, implementering, vedlikehold og support ...! ",
+     "n" => "Navn",
+     "a" => "Adresse",
+     "e" => "E-post",
+     "h3contact" => "Kontakt meg",
+     "h4contact" => "Kontakt meg i dag, og få svar med om 24 timer!",
+     "trmessage" => "Melding send, takk for at du kontaktet meg!",
+     "send" => "Sende melding ...",
+     "name1" => "Navnet ditt",
+     "email1" => "Din e-post",
+     "subject1" => "Emne",
+     "textarea1" => "Melding her ..., beskriv prosjekt ..?",
+
+
+
+
+);
+
+
+
+
+
+?>
